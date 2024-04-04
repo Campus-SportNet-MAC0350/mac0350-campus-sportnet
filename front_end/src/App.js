90e5ace4321c8345a2c4afc95661fa8ea9cf5184
Rodrigo Michelassi <rodrigomichelassi@APOLO.local>
2024-04-04 14:52:22 -0300

    add react for front_end

---
+++ front_end/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    // todo codigo do react fica dentro dessa funcao app
+    <div className="App">
+        <p>Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
